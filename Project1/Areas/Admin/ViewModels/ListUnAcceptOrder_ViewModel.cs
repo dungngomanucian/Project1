dece9757395fd0995e014e5dd6b037e643f44d62
quyet212606016@lms.utc.edu.vn <quyet212606016@lms.utc.edu.vn>
2024-12-08 16:14:48 +0700

    Quyết sửa, cập nhật: - Update lại layout của admin - Quản lý đơn hàng (thêm một số tính năng) - Quản lý đơn hàng cũ và mới (mới hoàn toàn)

---
+++ Project1/Areas/Admin/ViewModels/ListUnAcceptOrder_ViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Project1.Models;
+
+namespace Project1.Areas.Admin.ViewModels
+{
+    public class ListUnAcceptOrder_ViewModel
+    {
+        public List<TOrder> ListUnAcceptOrder { get; set; }
+    }
+}
