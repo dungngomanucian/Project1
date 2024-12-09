f95e75ac8e701ca15d2813246b8aa131310d3c38
quyet212606016@lms.utc.edu.vn <quyet212606016@lms.utc.edu.vn>
2024-12-09 21:11:24 +0700

    Quyết sửa và cập nhật: + Tên một số PartialView + Chức năng quản lý voucher + Hiển thị email người đăng nhập (có cập nhật file Program.cs)

--- Project1/Areas/Admin/Controllers/HomeAdminController.cs
+++ Project1/Areas/Admin/Controllers/HomeAdminController.cs
@@ -10,42 +10,46 @@ using Azure;
 using System.Diagnostics.Eventing.Reader;
 using Project1.Areas.Admin.ViewModels;
 using NuGet.Protocol;
+using Microsoft.AspNetCore.Authorization;
+using System.Drawing.Printing;
+using System.Collections.Generic;
+using X.PagedList.Extensions;
 
 namespace Project1.Areas.Admin.Controllers
 {
-
-    [Area("admin")]
-    [Route("admin")]
-    [Route("admin/homeadmin")]
-    public class HomeAdminController : Controller
-    {
+	
+	[Area("admin")]
+	[Route("admin")]
+	[Route("admin/homeadmin")]
+	public class HomeAdminController : Controller
+	{
         private readonly IWebHostEnvironment _webHostEnvironment;
         public HomeAdminController(IWebHostEnvironment webHostEnvironment)
         {
             _webHostEnvironment = webHostEnvironment;
         }
 
-        PizzaOnlineContext db = new PizzaOnlineContext();
-        [Authentication]
+        PizzaOnlineContext db= new PizzaOnlineContext();
+        
         [Route("")]
-        [Route("index")]
-        public IActionResult Index()
-        {
-
+		[Route("index")]
+		public IActionResult Index()
+		{
+           
             return View();
-        }
+		}
 
-        //từ đây trở xuống là các action phục vụ cho quản lý sản phẩm
-        [Route("ListProduct")]
-        public IActionResult ListProduct(int? page)
-        {
-
+		//từ đây trở xuống là các action phục vụ cho quản lý sản phẩm
+		[Route("ListProduct")]
+		public IActionResult ListProduct(int? page)
+		{
+            
             int pageSize = 9;
-            int pageNumber = page == null || page < 0 ? 1 : page.Value;
-            var lstProduct = db.TProducts.AsNoTracking().Include(x => x.Category).OrderBy(x => x.Deleted);
-            PagedList<TProduct> lst = new PagedList<TProduct>(lstProduct, pageNumber, pageSize);
-            return View(lst);
-        }
+			int pageNumber =page ==null || page<0 ? 1:page.Value;	
+			var lstProduct = db.TProducts.AsNoTracking().Include(x=>x.Category).OrderBy(x=>x.Deleted);
+			PagedList<TProduct> lst= new PagedList<TProduct>(lstProduct,pageNumber,pageSize);
+			return View(lst);
+		}
 
         /*  [Route("ListProductOptimizeByCategory")]
           public IActionResult ListProductOptimizeByCategory(int? page,string categoryId)
@@ -63,24 +67,24 @@ namespace Project1.Areas.Admin.Controllers
         {
             ViewBag.tukhoa = search_Keyword;
             long change = 0;
-            if (long.TryParse(search_Keyword, out change))
+            if(long.TryParse(search_Keyword, out change))
             {
                 int pageSize = 9;
                 int pageNumber = page == null || page < 0 ? 1 : page.Value;
                 var lstProduct = db.TProducts.AsNoTracking().Include(x => x.Category).Where(x => x.CategoryId == change).OrderBy(x => x.Name);
-                PagedList<TProduct> lst = new PagedList<TProduct>(lstProduct, pageNumber, pageSize);
+                PagedList<TProduct> lst = new PagedList<TProduct>(lstProduct, pageNumber, pageSize);                
                 return View(lst);
             }
             else
             {
                 int pageSize = 9;
                 int pageNumber = page == null || page < 0 ? 1 : page.Value;
-                var lstProduct = db.TProducts.AsNoTracking().Include(x => x.Category).Where(x => x.Name.Contains(search_Keyword)).OrderBy(x => x.Name);
+                var lstProduct = db.TProducts.AsNoTracking().Include(x => x.Category).Where(x => x.Name.Contains( search_Keyword)).OrderBy(x => x.Name);
                 PagedList<TProduct> lst = new PagedList<TProduct>(lstProduct, pageNumber, pageSize);
                 return View(lst);
             }
 
-
+            
         }
 
         [Route("AddProduct")]
@@ -121,7 +125,7 @@ namespace Project1.Areas.Admin.Controllers
                     viewModel.Product.LastModifiedDate = DateTime.UtcNow;
                     viewModel.Product.Deleted = false;
                     int lastProductDetailId = (int)db.TProductDetails.DefaultIfEmpty().Max(p => (int?)p.ProductDetailId ?? 0);
-                    viewModel.ProductDetail.ProductDetailId = lastProductDetailId + 1;
+                    viewModel.ProductDetail.ProductDetailId= lastProductDetailId+1;
                     viewModel.ProductDetail.ProductId = viewModel.Product.ProductId;
                     viewModel.ProductDetail.CreatedBy = user.Nickname;
                     viewModel.ProductDetail.CreatedDate = DateTime.UtcNow;
@@ -236,7 +240,7 @@ namespace Project1.Areas.Admin.Controllers
 
         //action trả về view hiển thị danh sách các hoá đơn mới
         [Route("ListUnAcceptOrder")]
-        public IActionResult ListUnAcceptOrder()
+        public IActionResult ListUnAcceptOrder( )
         {
             return View();
         }
@@ -255,7 +259,7 @@ namespace Project1.Areas.Admin.Controllers
                                  .ToList();
 
             // Trả về view với danh sách hoá đơn đã lọc
-            return PartialView("PartialView/_OrderList", orders);
+            return PartialView("PartialView/_OrderListbyState", orders);
         }
 
         //action với mục đích show form hiển thị chi tiết đơn hàng
@@ -264,7 +268,7 @@ namespace Project1.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetOrderDetail(int orderId)
         {
-            var order = db.TOrders.Include(x => x.Voucher).Where(od => od.OrderId == orderId).FirstOrDefault();
+            var order= db.TOrders.Include(x=>x.Voucher).Where(od=>od.OrderId == orderId).FirstOrDefault();
             double? total_price = order.TotalPrice;
             // Lấy thông tin chi tiết Order
             var orderDetails = (from odd in db.TOrderDetails
@@ -279,14 +283,14 @@ namespace Project1.Areas.Admin.Controllers
                                     CategoryName = ca.Name,
                                     ProductDetailId = pd.ProductDetailId,
                                     ProductName = p.Name, // Lấy tên sản phẩm từ bảng Product
-                                    Size = s.Name,
-                                    Crust = c.Name,
+                                    Size=s.Name,
+                                    Crust=c.Name,
                                     Price = pd.Price,
                                     Quantity = odd.Number, // Lấy số lượng từ OrderDetail
                                     ProductImage = p.Image // Lấy ảnh sản phẩm từ bảng Product
                                 }).ToList();
 
-
+            
 
             // Lấy thông tin Combo (nếu có)
             var orderCombo = db.TOrderCombos
@@ -297,46 +301,159 @@ namespace Project1.Areas.Admin.Controllers
             var viewModel = new OrderDetail_ViewModel
             {
                 SelectedProducts = orderDetails,
-                Order = order,
+                Order=order,
                 OCombo = orderCombo,
-                TotalPriceAfterUseVoucher = total_price
+                TotalPriceAfterUseVoucher=total_price
             };
 
             return PartialView("PartialView/_OrderDetail", viewModel);
         }
 
-
+        //action với mục đích show bảng liệt kê các đơn hàng cũ có trạng thái: đã giao hoặc huỷ
         [Route("ListOldOrder")]
         public IActionResult ListOldOrder(int? page)
         {
 
             int pageSize = 9;
             int pageNumber = page == null || page < 0 ? 1 : page.Value;
-            var listOOrder = db.TOrders.AsNoTracking().Include(x => x.Status).Include(x => x.PaymentMethod).Include(x => x.Manager).Where(x => x.StatusId == 5).ToList();
+            var listOOrder = db.TOrders.AsNoTracking().Include(x => x.Status).Include(x => x.PaymentMethod).Include(x => x.Manager).Where(x => x.StatusId == 5 || x.StatusId==6).OrderBy(x => x.CreatedDate).ToList();
             PagedList<TOrder> lst = new PagedList<TOrder>(listOOrder, pageNumber, pageSize);
             return View(lst);
         }
+
+        //action với mục đích tạo partialview điền vào phần chứa nội dung của view ListOldOrder
         [Route("searchOrder")]
         public IActionResult searchOrder(int? page, string search_Keyword)
         {
-            ViewBag.tukhoa = search_Keyword;
+            ViewBag.searchKeyword = search_Keyword;
             long change = 0;
+
+            // Kiểm tra nếu từ khóa là một số
             if (long.TryParse(search_Keyword, out change))
             {
-                int pageSize = 9;
+                var lstOrder = db.TOrders
+                                 .AsNoTracking().Include(x=>x.Status).Include(x=>x.PaymentMethod)
+                                 .Where(x => x.OrderId == change && (x.StatusId == 5 || x.StatusId == 6))
+                                 .ToList();
+
+                if (lstOrder.Count() == 0) // Không tìm thấy
+                {
+                    ViewBag.NoResult = "Hoá đơn bạn tìm kiếm không có trong dữ liệu.";
+                }
+                int pageSize = 1;
                 int pageNumber = page == null || page < 0 ? 1 : page.Value;
-                var lstOrder = db.TOrders.AsNoTracking().Where(x => x.OrderId == change).ToList();
                 PagedList<TOrder> lst = new PagedList<TOrder>(lstOrder, pageNumber, pageSize);
-                return View(lst);
+                return PartialView("PartialView/_OrderListAfterSearch", lst);
+
+                
+            }
+            else //Từ khoá không là 1 số
+            {
+                ViewBag.saidinhdang = "Mã hoá đơn nhập vào của bạn không là một số!";
+                return PartialView("PartialView/_OrderListAfterSearch");
+            }
+        }
+
+        [Route("FilterOrder")]
+        [HttpGet]
+        public IActionResult FilterOrder(string filterType, string filterValue, int? page)
+        {
+            // Bắt đầu query từ bảng TOrder
+            var orders = db.TOrders
+                .Include(o => o.PaymentMethod) // Include để lấy thông tin phương thức thanh toán
+                .Include(o => o.Status).
+                Where(o=>o.StatusId==5 || o.StatusId==6)// Include để lấy thông tin trạng thái
+                .AsQueryable();
+
+            // Lọc theo filterType và filterValue
+            if (!string.IsNullOrEmpty(filterType) && !string.IsNullOrEmpty(filterValue))
+            {
+                if (filterType == "paymentMethod")
+                {
+                    orders = orders.Where(o => o.PaymentMethod.Name == filterValue);
+                }
+                else if (filterType == "status")
+                {
+                    long change=long.Parse(filterValue);
+                    orders = orders.Where(o => o.StatusId == change);
+                }
+            }
+
+            // Phân trang
+            int pageSize = 8; // Số hóa đơn trên mỗi trang
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            PagedList<TOrder> lst = new PagedList<TOrder>(orders, pageNumber, pageSize);
+            
+
+            // Ghi nhận thông tin lọc vào ViewBag để phân trang giữ được trạng thái
+            ViewBag.FilterType = filterType;
+            ViewBag.FilterValue = filterValue;
+
+            // Trả về PartialView với danh sách hóa đơn được lọc
+            return PartialView("PartialView/_OrderListAfterFilter", lst);
+        }
+        //kết thúc các action xử lý hoá đơn
+
+
+
+        //từ đây trở xuống là các action xử lý voucher
+        [Route("ListVoucher")]
+        public IActionResult ListVoucher(int state)
+        {
+            if( state == 1)
+            {
+                var listVoucher = db.TVouchers.AsNoTracking().Where(x => x.Deleted == false && x.Number > 0).ToList();
+                ViewBag.Null = "List thành công";
+                return View(listVoucher);
+            }
+            else if (state==2)
+            {
+
+                var listVoucher = db.TVouchers.AsNoTracking().Where(x => x.Deleted == true || x.Number == 0).ToList();
+                if(listVoucher.Count()==0) { ViewBag.Null = "Không có voucher nào bị hết hạn hoặc hết số lượng"; }
+                return View(listVoucher);
             }
             else
             {
                 return View();
-                ViewBag.saidinhdang = "Mã hoá đơn nhập vào của bạn không là một số!";
             }
-
-
         }
-        //kết thúc các action xử lý hoá đơn
+
+        [Route("AddVoucher")]
+        [HttpGet]
+        public IActionResult AddVoucher()
+        {
+            
+            return View();
+        }
+
+        [Route("AddVoucher")]
+        [HttpPost]
+        public IActionResult AddVoucher(TVoucher voucher)
+        {
+            long change = long.Parse(HttpContext.Session.GetString("UserId"));
+            var user = db.TUsers.FirstOrDefault(x => x.UserId == change);
+            int lastVoucherId = (int)db.TVouchers.DefaultIfEmpty().Max(p => (int?)p.VoucherId ?? 0);
+            voucher.VoucherId = lastVoucherId + 1;
+            voucher.CreatedBy = user.Nickname;
+            voucher.CreatedDate = DateTime.UtcNow;
+            voucher.LastModifiedBy = user.Nickname;
+            voucher.LastModifiedDate = DateTime.UtcNow;
+            voucher.Deleted = false;
+            if (voucher != null)
+            {
+                db.TVouchers.Add(voucher);
+                db.SaveChanges();
+                return RedirectToAction("ListVoucher");
+            }
+            return View(voucher);
+        }
+        //kết thúc các action xử lý voucher
+
+        //từ đây trở xuống là các action xử lý combo
+        //kết thúc các action xử lý combo
+
+        //từ đây trở xuống là các action xử lý user
+        //kết thúc các action xử lý user
     }
 }
